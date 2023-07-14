9d24a535e883ee13a3fbad8ebc357f0b3e8f2aea
Vasyan <vasyan.coder@gmail.com>
2023-07-14 21:28:55 +0300

    feat: added security

---
+++ src/main/java/com/vasyancoder/HackProd/security/SecurityService.java
@@ -0,0 +1,81 @@
+package com.vasyancoder.HackProd.security;
+
+import com.vasyancoder.HackProd.entity.UserEntity;
+import com.vasyancoder.HackProd.exception.AuthException;
+import com.vasyancoder.HackProd.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import javax.security.sasl.AuthenticationException;
+import java.util.*;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Integer expirationInSeconds;
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    private TokenDetails generateToken(UserEntity user) {
+        Map<String, Object> claims = new HashMap<>() {{
+            put("role", user.getRole());
+        }};
+        return generateToken(claims, user.getId().toString());
+    }
+
+    private TokenDetails generateToken(Map<String, Object> claims, String subject) {
+        Long expirationTimeInMillis = expirationInSeconds * 1000L;
+        Date expirationDate = new Date(new Date().getTime() + expirationTimeInMillis);
+
+        return generateToken(expirationDate, claims, subject);
+    }
+
+    private TokenDetails generateToken(Date expirationDate, Map<String, Object> claims, String subject) {
+        Date createdData = new Date();
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuer(issuer)
+                .setSubject(subject)
+                .setIssuedAt(createdData)
+                .setId(UUID.randomUUID().toString())
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encodeToString(secret.getBytes()))
+                .compact();
+
+        return TokenDetails.builder()
+                .token(token)
+                .issuedAt(createdData)
+                .expiresAt(expirationDate)
+                .build();
+    }
+
+    public Mono<TokenDetails> authenticate(String username, String password) {
+        return userRepository.findByUsername(username)
+                .flatMap(user -> {
+                    if (!user.isEnabled()) {
+                        return Mono.error(new AuthException("Account disabled", "PROSELYTE_USER_ACCOUNT_DISABLE"));
+                    }
+
+                    if (passwordEncoder.matches(password, user.getPassword())) {
+                        return Mono.error(new AuthException("Invalid password", "PROSELYTE_INVALID_PASSWORD"));
+                    }
+
+                    return Mono.just(generateToken(user).toBuilder()
+                            .userId(user.getId())
+                            .build());
+                })
+                .switchIfEmpty(Mono.error(new AuthException("Invalid username", "PROSELYTE_INVALID_USERNAME")));
+    }
+}
