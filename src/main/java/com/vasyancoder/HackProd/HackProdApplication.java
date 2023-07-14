d0916a981a4913007e138e784c51322d057a9632
Vasyan <vasyan.coder@gmail.com>
2023-07-14 19:24:02 +0300

    init

---
+++ src/main/java/com/vasyancoder/HackProd/HackProdApplication.java
@@ -0,0 +1,13 @@
+package com.vasyancoder.HackProd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackProdApplication.class, args);
+	}
+
+}
