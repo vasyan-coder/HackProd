e73f35a5bee7570f0c47363048c6e6f01fbbb5e2
Vasyan <vasyan.coder@gmail.com>
2023-07-15 11:44:21 +0300

    feat: added error handling

--- src/main/java/com/vasyancoder/HackProd/exception/ApiException.java
+++ src/main/java/com/vasyancoder/HackProd/exception/ApiException.java
@@ -1,7 +1,10 @@
 package com.vasyancoder.HackProd.exception;
 
+import lombok.Getter;
+
 public class ApiException extends RuntimeException {
 
+    @Getter
     protected String errorCode;
 
     public ApiException(String message, String errorCode) {
